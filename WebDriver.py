b65383421166ffd8a5dcda2431f8f32ed2e1b037
jarvis <justin.needham@alumnimail.pepperdine.edu>
2018-01-20 12:36:13 -0700

    Calculator V1.0.0

--- WebDriver.py
+++ WebDriver.py
@@ -1,3 +1,6 @@
+# Copyright Alexandria Books All Rights Reserved
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
